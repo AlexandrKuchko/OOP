53a0ca627fcfc3baddd7111e50760b1244dfba54
Alexandr Kuchko <sashakuchko@yandex.ru>
2021-02-15 20:34:34 +0800

    feature(LR2): начало работы

---
+++ LR2/LR2.OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LR2.OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
