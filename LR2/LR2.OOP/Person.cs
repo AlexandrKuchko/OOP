83be6bc2faa96deb01cabaa2bd958c4acd03caa8
Alexandr Kuchko <sashakuchko@yandex.ru>
2021-02-16 10:45:12 +0800

    feature(LR2): добавлены класссы Peson и PersonList

---
+++ LR2/LR2.OOP/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR2.OOP
+{
+    class Person
+    {
+
+        public enum Gender
+        {
+            Men,
+            Woman
+        }
+
+        public string Name;
+        public string Surname;
+        public int Age;
+        public Gender sex;
+       
+    
+
+        public Person (string a1, string a2, int a3, Gender sex)
+        {
+            Name = a1;
+            Surname = a2;
+            Age = a3;
+            this.sex = sex;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ LR2/LR2.OOP/PersonList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR2.OOP
+{
+    class Class2
+    {
+    }
+}
